3e622e95d3fa8a1ba9c26985b3be16fcfa9ed266
Justin Liu <justin.xin.liu@gmail.com>
2017-05-16 12:31:36 +1000

    New navigation project with NavigationView.

---
+++ Sample.Navigation/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+
+namespace Sample.Navigation
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // The following cannot be done in XAML in this build yet.
+            MyNavigationView.AddMenuItem(Symbol.AllApps, "All Apps", (s, e) => RootFrame.Navigate(typeof(AppsPage)), true);
+            MyNavigationView.AddMenuItem(Symbol.Video, "Games", (s, e) => { });
+            MyNavigationView.AddMenuItem(Symbol.Calendar, "Calendar", (s, e) => { });
+            MyNavigationView.AddMenuItemSeparator();
+            MyNavigationView.AddMenuItem(Symbol.Admin, "My Account", (s, e) => { });
+            
+            RootFrame.Navigate(typeof(AppsPage));
+        }
+    }
+
+    public static class NavigationViewExtensions
+    {
+        public static void AddMenuItem(this NavigationView navigationView, Symbol icon, string text,
+            TypedEventHandler<NavigationMenuItem, object> handler, bool selected = false)
+        {
+            var item = new NavigationMenuItem
+            {
+                Icon = new SymbolIcon(icon),
+                Text = text,
+                IsSelected = selected
+            };
+            item.Invoked += handler;
+            navigationView.MenuItems.Add(item);
+        }
+
+        public static void AddMenuItemSeparator(this NavigationView navigationView)
+        {
+            var item = new NavigationMenuItemSeparator();
+            navigationView.MenuItems.Add(item);
+        }
+    }
+}
