77d83b1d5f2e464467696766a658d0953d687035
Justin Liu <justin.xin.liu@gmail.com>
2017-07-15 11:21:44 +1000

    Updated to Insider sdk 16232.

---
+++ Continuity/Controls/FluentNavigationView/FluentNavigationViewItem.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Continuity.Controls
+{
+    public sealed class FluentNavigationViewItem : NavigationViewItem
+    {
+        public FluentNavigationViewItem()
+        {
+            DefaultStyleKey = typeof(FluentNavigationViewItem);
+        }
+    }
+}
